09e0569d246cc37f3abc5bea0b64b6901bfd1d4b
pudep <261843105+pudep@users.noreply.github.com>
2026-07-10 15:26:45 +0530

    removed input key

--- src/keys/input.rs
+++ src/keys/input.rs
@@ -7,17 +7,9 @@ use std::error::Error;
 pub fn handle_input(app: &mut App) -> std::result::Result<Action, Box<dyn Error>> {
   if let Event::Key(key) = crossterm::event::read()? {
     match key.code {
-      KeyCode::Enter if app.current_tab == TabPage::HomePage => match app.menu.selected {
-        0 => return Ok(Action::EnterContinue),
-        1 => return Ok(Action::EnterSetting),
-        2 => return Ok(Action::Quit),
-        _ => {}
-      },
       KeyCode::Up if app.current_tab == TabPage::HomePage => {
-        app.menu.select_prev();
       }
       KeyCode::Down if app.current_tab == TabPage::HomePage => {
-        app.menu.select_next();
       }
       KeyCode::Char('q') if key.modifiers == KeyModifiers::CONTROL => return Ok(Action::Quit),
 
