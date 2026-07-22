850dba753fd36381987dbc9aa81e57edce2f85b1
pudep <261843105+pudep@users.noreply.github.com>
2026-07-22 08:00:30 +0530

    removed commenting

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,16 +1,12 @@
 use git2::{Branch, Commit, Reference, Repository};
 
 pub mod current;
-pub mod string_to_path;
 pub mod refresh;
+pub mod string_to_path;
 
-/// Precidence : 0
-/// Core struct -> holds expensive vars
-/// It can't be life time bounded
 #[allow(dead_code)]
 pub struct Git {
   pub repo: Repository,
-  // pub state: RepositoryState,
 }
 
 pub enum RepoState {
@@ -27,11 +23,6 @@ pub enum RepoState {
   MailBoxOrRebase,
 }
 
-/// Precidence : 1st
-/// If current branch -> ref head
-/// else : `Commit<'repo>`
-/// If Unborn -> `Unborn`
-/// Rare : Error
 #[allow(dead_code)]
 pub enum Head<'repo> {
   Refrence(Reference<'repo>),
@@ -40,18 +31,12 @@ pub enum Head<'repo> {
   Unborn,
 }
 
-/// Precidence: 2nd
-/// If Head::Refrence(reference) -> `Branch<'repo>`
-/// else -> `Error(String)`
 #[allow(dead_code)]
 pub enum Local<'repo> {
   Branch(Branch<'repo>),
   Error(String),
 }
 
-/// Precidence : 3rd
-/// If `Local::Branch(Branch<'repo>)` -> `Commit<'repo>`.
-/// else -> Error
 #[allow(dead_code)]
 pub enum Upstream<'repo> {
   Branch(Branch<'repo>),
