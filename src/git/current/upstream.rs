7b6af9e9360e760d6520bb0201463560d67808e3
pudep <261843105+pudep@users.noreply.github.com>
2026-07-22 21:26:25 +0530

    fixed upstream

--- src/git/current/upstream.rs
+++ src/git/current/upstream.rs
@@ -1,38 +1,38 @@
-use git2::{Branch, Commit};
+use anyhow::Ok;
+use git2::{Branch, Oid, Repository};
 
-use crate::git::Git;
+use crate::git::{Git, current::local::Local};
 
 #[allow(dead_code)]
-pub enum Upstream<'repo> {
-  Branch(Branch<'repo>),
-  Commit(Commit<'repo>),
+pub enum Upstream {
+  Branch(String),
+  Oid(Oid),
   Error(String),
   None,
 }
 
 impl Git {
-  // pub fn get_oid_current<'repo>(
-  //   repo: &'repo Repository,
-  //   current: &Local,
-  // ) -> anyhow::Result<Upstream<'repo>, anyhow::Error> {
-  //   match current {
-  //     Local::Branch(b) => match b.upstream() {
-  //       Ok(localbranch) => Ok(Upstream::Commit(
-  //         repo.find_commit(localbranch.get().target().unwrap())?,
-  //       )),
-  //       Err(e) => Ok(Upstream::Error(e.to_string())),
-  //     },
-  //     Local::Error(error) => Ok(Upstream::Error(error.to_string())),
-  //   }
-  // }
+  pub fn get_upstream_oid(
+    repo: &Repository,
+    local: &Local,
+    branch: &Branch,
+  ) -> anyhow::Result<Upstream, anyhow::Error> {
+    if local.is_branch() {
+      return Ok(Upstream::Oid(
+        repo.find_commit(branch.get().target().unwrap())?.id(),
+      ));
+    } else {
+      return Ok(Upstream::None);
+    }
+  }
 
-  // pub fn get_current_upstream(current: &Local) -> Upstream {
-  //   // match &current {
-  //   //   Local::Branch(local_branch) => match local_branch.upstream() {
-  //   //     Ok(k) => Upstream::Branch(k),
-  //   //     Err(e) => Upstream::Error(e.to_string()),
-  //   //   },
-  //   //   _ => Upstream::None,
-  //   // }
-  // }
+  pub fn get_upstream(local: &Local, branch: &Branch) -> anyhow::Result<Upstream, anyhow::Error> {
+    if local.is_branch() {
+      return Ok(Upstream::Branch(
+        branch.upstream()?.name()?.unwrap().to_string(),
+      ));
+    } else {
+      return Ok(Upstream::None);
+    }
+  }
 }
--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,6 +1,6 @@
 use git2::Repository;
 
-use crate::git::current::{head::Head, local::Local};
+use crate::git::current::{head::Head, local::Local, upstream::Upstream};
 pub mod current;
 pub mod string_to_path;
 
@@ -9,6 +9,7 @@ pub struct Git {
   pub repo: Repository,
   pub head: Head,
   pub local: Local,
+  pub upstream: Upstream,
 }
 
 #[allow(dead_code)]
@@ -17,6 +18,18 @@ impl Git {
     let repo = Repository::open(Git::string_to_path(path)?)?;
     let head = Git::get_current_head(&repo)?;
     let local = Git::get_current_local_branch(&head, &repo)?;
-    Ok(Self { repo, head, local })
+    let upstream = {
+      let local_branch = match &local {
+        Local::Branch(name) => Some(repo.find_branch(name, git2::BranchType::Local)?),
+        _ => None,
+      };
+      Git::get_upstream(&local, local_branch.as_ref().unwrap())?
+    };
+    Ok(Self {
+      repo,
+      head,
+      local,
+      upstream,
+    })
   }
 }
