ee5e9fbc68a1a818c67369b97bab74f42d82aa66
pudep <261843105+pudep@users.noreply.github.com>
2026-07-25 15:55:53 +0530

    Config code works as expected

--- src/bin/git.rs
+++ src/bin/git.rs
@@ -1,20 +1,19 @@
-use pugit::git::{
-  Git,
-  current::{
-    upstream::{Upstream},
-  },
-};
+use pugit::git::{Git, current::upstream::Upstream};
 
 fn main() -> anyhow::Result<()> {
-  let git = Git::new("~/impl/rust/pucrypt/")?;
+  let git = Git::new("~/impl/rust/pugit/")?;
 
   match &git.upstream {
     Upstream::Found(name) => {
       let oid = Upstream::get_oid(&git.repo, &Upstream::to_branch(name, &git.repo)?)?;
       println!("upstream branch: {name}\nUpstream Oid: {oid}")
-    },
+    }
     Upstream::NotFound => println!("no upstream configured"),
     Upstream::Error(e) => println!("upstream error: {e}"),
+  }
+
+  for (i, v) in &git.config.config {
+    println!("{i} --> {v}");
   }
 
   Ok(())
