c8f30d11f8725ef1b4c8554c82da5f1009cd0942
pudep <261843105+pudep@users.noreply.github.com>
2026-07-26 08:52:02 +0530

    added doc comments for src/git/mod.rs

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,14 +1,19 @@
+// ==========================
 use git2::Repository;
-
 use crate::git::current::{
   config::{self, Config},
   head::Head,
   local::Local,
   upstream::Upstream,
 };
+// ==========================
+
+// // //  //  //
 pub mod current;
 pub mod string_to_path;
+// // //  //  //
 
+/// Pugit's core data sturcture that holds almost all important Git things.
 #[allow(dead_code)]
 pub struct Git {
   pub repo: Repository,
@@ -20,6 +25,8 @@ pub struct Git {
 
 #[allow(dead_code)]
 impl Git {
+  /// Compiles everything into a single structure.
+  /// Can be used for Repowide refresh if called again.
   pub fn new(path: &str) -> anyhow::Result<Self> {
     let repo = Repository::open(Git::string_to_path(path)?)?;
     let head = Head::new(&repo)?;
