67546324a2733789cbf4178c446fd1a70b6ffd35
pudep <261843105+pudep@users.noreply.github.com>
2026-07-27 13:06:38 +0530

    Added: Code that returns Vec<FileStatus> to src/git/current/index.rs

--- src/git/current/index.rs
+++ src/git/current/index.rs
@@ -1,10 +1,75 @@
+use std::path::PathBuf;
 use git2::Repository;
 
-pub fn index(repo: &Repository) -> anyhow::Result<()> {
-  let idx = repo.index()?;
-  for i in idx.iter() {
-    let path = String::from_utf8_lossy(&i.path);
-    println!("{}, {}, {}", i.id, i.mode, path);
+/// `repo.statuses()` returns `Statuses`
+/// Statuses returns files with status.
+/// This enum is to mark status of a file that contains status.
+#[allow(dead_code)]
+pub enum FileState {
+  Modified,
+  Renamed,
+  Deleted,
+  Added,
+  Untracked,
+  Conflicted,
+  Ignored,
+  Empty,
+  TypeChanged,
+}
+
+/// Status of a File including:
+/// Path and FileState
+/// This field must be used as -> Vec<FileStatus>
+#[allow(dead_code)]
+pub struct FileStatus {
+  pub path: PathBuf,
+  pub state: FileState,
+}
+
+#[allow(dead_code)]
+impl FileStatus {
+  fn insert(path: String, state: FileState) -> Self {
+    let path = PathBuf::from(path);
+    Self { path, state }
   }
-  Ok(())
+}
+
+#[allow(dead_code)]
+impl FileState {
+  fn new(repo: &Repository) -> anyhow::Result<Vec<FileStatus>> {
+    let mut status_opts = git2::StatusOptions::new();
+    status_opts
+      .include_untracked(true)
+      .include_ignored(true)
+      .renames_head_to_index(true)
+      .renames_index_to_workdir(true)
+      .recurse_untracked_dirs(true);
+    let statuses = repo.statuses(Some(&mut status_opts))?;
+    let mut vector = Vec::new();
+    for entry in statuses.iter() {
+      let status = entry.status();
+      let path = entry.path().unwrap_or("").to_string();
+
+      if status.is_wt_new() {
+        vector.push(FileStatus::insert(path, Self::Untracked))
+      } else if status.is_wt_modified() || status.is_index_modified() {
+        vector.push(FileStatus::insert(path, Self::Modified))
+      } else if status.is_index_new() {
+        vector.push(FileStatus::insert(path, Self::Added))
+      } else if status.is_wt_deleted() || status.is_index_deleted() {
+        vector.push(FileStatus::insert(path, Self::Deleted))
+      } else if status.is_wt_renamed() || status.is_index_renamed() {
+        vector.push(FileStatus::insert(path, Self::Renamed))
+      } else if status.is_conflicted() {
+        vector.push(FileStatus::insert(path, Self::Conflicted));
+      } else if status.is_ignored() {
+        vector.push(FileStatus::insert(path, Self::Ignored));
+      } else if status.is_empty() {
+        vector.push(FileStatus::insert(path, Self::Empty));
+      } else if status.is_index_typechange() || status.is_wt_typechange() {
+        vector.push(FileStatus::insert(path, Self::TypeChanged));
+      }
+    }
+    Ok(vector)
+  }
 }
