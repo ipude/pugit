fc9de460159cbf18fd0f650e7b97ed39e4741219
pudep <261843105+pudep@users.noreply.github.com>
2026-07-29 16:57:56 +0530

    corrected vague comments

--- src/git/current/head.rs
+++ src/git/current/head.rs
@@ -2,13 +2,13 @@ use git2::Oid;
 use git2::{ErrorCode, Repository};
 
 /// Returns either of :
-/// `Refrence(String)` -- Attached head's branch name
+/// `Branch(String)` -- Attached head's branch name
 /// `Detached(Oid)` -- Detached head's oid
 /// `Error(String)` -- Real error
-/// `Unborn` -- When is unborn.
+/// `Unborn` -- When head is unborn.
 #[allow(dead_code)]
 pub enum Head {
-  Refrence(String),
+  Branch(String),
   Detached(Oid),
   Error(String),
   Unborn,
@@ -17,9 +17,9 @@ pub enum Head {
 // Match handeling.
 #[allow(dead_code)]
 impl Head {
-  /// Returns true if `Head::Refrence(_)` matches.
-  pub fn is_refrence(&self) -> bool {
-    matches!(self, Head::Refrence(_))
+  /// Returns true if `Head::Branch(_)` matches.
+  pub fn is_branch(&self) -> bool {
+    matches!(self, Head::Branch(_))
   }
   /// Returns true if `Head::Detached(_)` matches.
   pub fn is_detached(&self) -> bool {
@@ -36,11 +36,11 @@ impl Head {
 }
 
 impl Head {
-  /// This function is intentionally made to give `Option<String>` i.e the name of `Head::Refrence(name)` if it exists.
-  /// This may panic on `.unwrap()` if `Head::Refrence()` is None.
+  /// This function is intentionally made to give `Option<String>` i.e the name of `Head::Branch(name)` if it exists.
+  /// This may panic on `.unwrap()` if `Head::Branch()` is None.
   pub fn get_value(&self) -> Option<String> {
     match self {
-      Head::Refrence(name) => Some(name.to_string()),
+      Head::Branch(name) => Some(name.to_string()),
       _ => None,
     }
   }
@@ -50,16 +50,16 @@ impl Head {
 impl Head {
   /// Retuns enum `Head`.
   /// May return :
-  /// `Refrence(String)` if head is a branch.
+  /// `Branch(String)` if head is a branch.
   /// `Detached(Oid)` if head is detached.
   /// `Unborn` if head is unborn.
   /// `Error` if somthing is wrong.
   pub fn new(repo: &Repository) -> anyhow::Result<Head, anyhow::Error> {
     match repo.head() {
       Ok(head) => {
-        // Note: This is repo.head()?.is_branch()
         if head.is_branch() {
-          Ok(Head::Refrence(head.shorthand()?.to_string()))
+          // Make sure it must be repo.shorthand()? not repo.name()?
+          Ok(Head::Branch(head.shorthand()?.to_string()))
         } else {
           match head.target() {
             Some(oid) => Ok(Head::Detached(repo.find_commit(oid)?.id())),
@@ -74,15 +74,4 @@ impl Head {
       Err(e) => Ok(Head::Error(e.to_string())),
     }
   }
-
-  // pub fn ahead_behind(repo: &Repository, head: &Head, local: &Local) -> anyhow::Result<()> {
-  //   match &head {
-  //     Head::Refrence(head) => {
-  //       // let local_oid = head.target().unwrap();
-  //       // let upstream_oid = Git::get_current_upstream(&local);
-  //     }
-  //     _ => {}
-  //   }
-  //   Ok(())
-  // }
 }
