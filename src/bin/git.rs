affebe18ea7b06cb952bbb5efcaa65a4ef3be008
pudep <261843105+pudep@users.noreply.github.com>
2026-07-29 16:10:02 +0530

    have succesfully implemented ahead_behind finder for two branches Main and New.

--- src/bin/git.rs
+++ src/bin/git.rs
@@ -1,106 +1,33 @@
-use git2::{Repository, StatusOptions, Statuses};
 use pugit::git::Git;
 
-fn index_char(s: git2::Status) -> char {
-  use git2::Status;
-  if s.contains(Status::INDEX_NEW) {
-    'A'
-  } else if s.contains(Status::INDEX_MODIFIED) {
-    'M'
-  } else if s.contains(Status::INDEX_DELETED) {
-    'D'
-  } else if s.contains(Status::INDEX_RENAMED) {
-    'R'
-  } else if s.contains(Status::INDEX_TYPECHANGE) {
-    'T'
-  } else {
-    ' '
-  }
-}
-
-fn wt_char(s: git2::Status) -> char {
-  use git2::Status;
-  if s.contains(Status::WT_MODIFIED) {
-    'M'
-  } else if s.contains(Status::WT_DELETED) {
-    'D'
-  } else if s.contains(Status::WT_TYPECHANGE) {
-    'T'
-  } else if s.contains(Status::WT_RENAMED) {
-    'R'
-  } else {
-    ' '
-  }
-}
-
-fn conflict_code(index: &git2::Index, path: &str) -> Option<&'static str> {
-  for c in index.conflicts().ok()?.flatten() {
-    if c
-      .our
-      .as_ref()
-      .or(c.their.as_ref())
-      .or(c.ancestor.as_ref())
-      .map(|e| e.path == path.as_bytes())
-      != Some(true)
-    {
-      continue;
-    }
-    return Some(
-      match (c.ancestor.is_some(), c.our.is_some(), c.their.is_some()) {
-        (true, false, false) => "DD",
-        (false, true, false) => "AU",
-        (true, true, false) => "UD",
-        (false, false, true) => "UA",
-        (true, false, true) => "DU",
-        (false, true, true) => "AA",
-        (true, true, true) => "UU",
-        _ => "??",
-      },
-    );
-  }
-  None
-}
-
-fn get_statuses<'repo>(repo: &'repo Repository) -> anyhow::Result<Statuses<'repo>, anyhow::Error> {
-  let mut opts = StatusOptions::new();
-  opts
-    .include_untracked(true)
-    .include_ignored(false)
-    .recurse_untracked_dirs(true)
-    .exclude_submodules(false)
-    .renames_head_to_index(true)
-    .renames_index_to_workdir(true)
-    .no_refresh(false);
-
-  Ok(repo.statuses(Some(&mut opts))?)
-}
-
+#[allow(dead_code)]
+#[allow(unused)]
 fn main() -> anyhow::Result<(), anyhow::Error> {
-  let git = Git::new("~/impl/rust/pugit/")?;
-  let repo = &git.repo;
-  let index = repo.index()?;
-  let statuses = get_statuses(repo)?;
-
-  for entry in statuses.iter() {
-    let path = entry.path().unwrap_or("<invalid utf-8>");
-    let status = entry.status();
-
-    let code = if status.contains(git2::Status::WT_NEW)
-      && !status.intersects(
-        git2::Status::INDEX_NEW
-          | git2::Status::INDEX_MODIFIED
-          | git2::Status::INDEX_DELETED
-          | git2::Status::INDEX_RENAMED
-          | git2::Status::INDEX_TYPECHANGE,
-      ) {
-      "??".to_string()
-    } else if status.is_conflicted() {
-      conflict_code(&index, path).unwrap_or("UU").to_string()
-    } else {
-      format!("{}{}", index_char(status), wt_char(status))
-    };
-
-    println!("{code} {path}");
+  let git = Git::new("~/tmp/")?;
+  let refs = git.repo.references_glob("refs/heads/**")?;
+  for r in refs {
+    let r = r?;
+    let name = r.name().unwrap_or("").to_string();
+    let target = r.target();
+    println!("{name} --> {:?}", target)
   }
+
+  let remotes = git.repo.remotes()?;
+  for i in remotes.iter() {
+    let i = i?.unwrap_or("");
+    let remote = git.repo.find_remote(i)?.name()?.unwrap_or("").to_string();
+    println!("{i}");
+    println!("{remote}");
+  }
+
+  let main = git.repo.find_branch("main", git2::BranchType::Local)?;
+  let new = git.repo.find_branch("new", git2::BranchType::Local)?;
+
+  let (ahead, behind) = git.repo.graph_ahead_behind(main.get().target().unwrap(), new.get().target().unwrap())?;
+
+  if ahead > behind {
+    println!("Branch: {} is ahead by {ahead} commits from  Branch: {}", main.name()?.unwrap_or("<no name>").to_uppercase(), new.name()?.unwrap_or("<no name>").to_uppercase())
+  }
+
   Ok(())
 }
