539af6f9e90e79149c2f2141dba6925b6f2b149e
pudep <261843105+pudep@users.noreply.github.com>
2026-07-29 16:57:22 +0530

    added ahead behind finder

---
+++ src/git/current/ahead_behind.rs
@@ -0,0 +1,18 @@
+use git2::{Branch, Repository};
+
+fn ahead_from_current(repo: &Repository, local_branch: &Branch) -> anyhow::Result<(), anyhow::Error> {
+  let main = repo.find_branch("main", git2::BranchType::Local)?;
+  let new = repo.find_branch("new", git2::BranchType::Local)?;
+
+  let (ahead, behind) =
+    repo.graph_ahead_behind(main.get().target().unwrap(), new.get().target().unwrap())?;
+
+  if ahead > behind {
+    println!(
+      "Branch: {} is ahead by {ahead} commits from  Branch: {}",
+      main.name()?.unwrap_or("<no name>").to_uppercase(),
+      new.name()?.unwrap_or("<no name>").to_uppercase()
+    )
+  }
+  Ok(())
+}
