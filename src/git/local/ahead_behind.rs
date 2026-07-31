354e35373577334feb0f4ab925e0ff3c26817b1c
pudep <261843105+pudep@users.noreply.github.com>
2026-07-31 19:34:16 +0530

    nothing much just some minor variable name change to src/git/local/ahead_behind.rs

--- src/git/local/ahead_behind.rs
+++ src/git/local/ahead_behind.rs
@@ -4,9 +4,9 @@ use crate::git::Git;
 
 #[allow(dead_code)]
 impl Git {
-  fn ahead_behind_from_current(
+  pub fn ahead_behind_from_current(
     repo: &Repository,
-    local_branch: &Branch,
+    current_branch: &Branch,
   ) -> anyhow::Result<(), anyhow::Error> {
     let main = repo.find_branch("main", git2::BranchType::Local)?;
     let new = repo.find_branch("new", git2::BranchType::Local)?;
