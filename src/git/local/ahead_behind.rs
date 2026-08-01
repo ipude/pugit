daf2103ed039f15a3ae7f423d7ee8a68c0332332
pudep <261843105+pudep@users.noreply.github.com>
2026-08-01 14:53:01 +0530

    iterating over Vec<BranchStatus> to get ahead_behind_graph

--- src/git/local/ahead_behind.rs
+++ src/git/local/ahead_behind.rs
@@ -1,25 +1,30 @@
 use git2::{Branch, Repository};
 
-use crate::git::Git;
+use crate::git::{Git, global::branches::BranchStatus};
 
 #[allow(dead_code)]
 impl Git {
   pub fn ahead_behind_from_current(
     repo: &Repository,
     current_branch: &Branch,
+    branches: &[BranchStatus],
   ) -> anyhow::Result<(), anyhow::Error> {
-    let main = repo.find_branch("main", git2::BranchType::Local)?;
-    let new = repo.find_branch("new", git2::BranchType::Local)?;
+    for branch in branches.iter() {
+      if branch.found() {
+        let b1 = Git::to_branch_local(repo, &branch.get_value())?;
+        let b2 = Git::to_branch_local(repo, &branches.iter().next().unwrap().get_value())?;
 
-    let (ahead, behind) =
-      repo.graph_ahead_behind(main.get().target().unwrap(), new.get().target().unwrap())?;
+        let (ahead, behind) =
+          repo.graph_ahead_behind(b1.get().target().unwrap(), b2.get().target().unwrap())?;
 
-    if ahead > behind {
-      println!(
-        "Branch: {} is ahead by {ahead} commits from  Branch: {}",
-        main.name()?.unwrap_or("<no name>").to_uppercase(),
-        new.name()?.unwrap_or("<no name>").to_uppercase()
-      )
+        print!(
+          "({}, {}):    ",
+          b1.name()?.unwrap_or("<no name>").to_uppercase(),
+          b2.name()?.unwrap_or("<no name>").to_uppercase()
+        );
+
+        println!("({}, {})", ahead, behind);
+      }
     }
     Ok(())
   }
