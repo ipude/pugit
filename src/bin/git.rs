4f3b58e81a6627ce9ee09e7a300a830720eba8d0
pudep <261843105+pudep@users.noreply.github.com>
2026-08-02 18:26:24 +0530

    reset bin/git.rs

--- src/bin/git.rs
+++ src/bin/git.rs
@@ -3,19 +3,6 @@ use pugit::git::Git;
 #[allow(dead_code)]
 #[allow(unused)]
 fn main() -> anyhow::Result<(), anyhow::Error> {
-  let git = Git::new("~/tmp/")?;
-  let refs = git.repo.references_glob("refs/heads/**")?;
-  for r in refs {
-    let r = r?;
-    let name = r.name().unwrap_or("").to_string();
-    let target = r.target();
-    println!("{name} --> {:?}", target)
-  }
-
-  for reference in git.repo.references_glob("refs/remotes/*")? {
-    let reference = reference?;
-    println!("{}", reference.name().unwrap_or("?"));
-  }
 
   Ok(())
 }
