458ec393479ce47d1a84025c26b8bbf4849dcd1e
pudep <261843105+pudep@users.noreply.github.com>
2026-08-06 10:05:13 +0530

    refactor: use new name and available fields (input.rs)

--- src/keys/input.rs
+++ src/keys/input.rs
@@ -1,14 +1,14 @@
 use crate::{
   action::navigate::Action,
-  state::tabs::{App, TabPage},
+  tui::{App, Pages},
 };
 use crossterm::event::{Event, KeyCode, KeyModifiers};
 use std::error::Error;
 pub fn handle_input(app: &mut App) -> std::result::Result<Action, Box<dyn Error>> {
   if let Event::Key(key) = crossterm::event::read()? {
     match key.code {
-      KeyCode::Up if app.current_tab == TabPage::HomePage => {}
-      KeyCode::Down if app.current_tab == TabPage::HomePage => {}
+      KeyCode::Up if app.active_page == Pages::HomePage => {}
+      KeyCode::Down if app.active_page == Pages::HomePage => {}
       KeyCode::Char('q') if key.modifiers == KeyModifiers::CONTROL => return Ok(Action::Quit),
 
       KeyCode::Char('?') => return Ok(Action::GoToHelpPage),
