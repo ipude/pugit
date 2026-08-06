75dcf464f964a3a0372312b9e651628720463bb8
pudep <261843105+pudep@users.noreply.github.com>
2026-08-06 10:04:45 +0530

    refactor: remove module state

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ mod action;
 mod cmd;
 mod git;
 mod keys;
-mod state;
+mod tui;
 mod watcher;
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
   cmd::parser();
-  let mut app = crate::state::tabs::App::new()?;
+  let mut app = crate::tui::App::new()?;
   app.run()?;
   Ok(())
 }
