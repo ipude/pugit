7aa70c8dc91554a7aec06a9125a009aa01890e33
pudep <261843105+pudep@users.noreply.github.com>
2026-08-06 17:05:08 +0530

    test: using struct Git

--- src/bin/git.rs
+++ src/bin/git.rs
@@ -1,5 +1,12 @@
+use fgit::git::Git;
+
 #[allow(dead_code)]
 #[allow(unused)]
 fn main() -> anyhow::Result<(), anyhow::Error> {
+  let git = Git::new("../")?;
+  if git.head.is_attached() {
+    let b = git.head.get_attached(&git.repo)?.unwrap();
+    println!("{}",b.name()?.unwrap());
+  }
   Ok(())
 }
