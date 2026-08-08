e08d1b6f9c2d212bbc9c4c8a8e566767cff9a481
pudep <261843105+pudep@users.noreply.github.com>
2026-08-08 11:53:46 +0530

    style: removed redundant inline comments.

--- src/git/ahead_behind.rs
+++ src/git/ahead_behind.rs
@@ -20,22 +20,23 @@ impl Git {
     branches: &[(usize, String)],
   ) -> anyhow::Result<Vec<ABData>, anyhow::Error> {
     let mut vector = Vec::new();
+
     for (_idx, branch) in branches {
       let other_branch = Git::to_local_branch(repo, &branch.as_str())?;
 
-      // To avoid recalculation everytime
       let current_branch_name = match current_branch.name() {
         Ok(Some(v)) => v.to_string(),
         Ok(None) => "<Invalid Utf-8>".to_string(),
         Err(_) => "<Invalid Branch/HEAD is detached>".to_string(),
       };
+
       let other_branch_name = match current_branch.name() {
         Ok(Some(v)) => v.to_string(),
         Ok(None) => "<Invalid Utf-8>".to_string(),
         Err(_) => "<Invalid Branch/HEAD is detached>".to_string(),
       };
 
-      // The filteration tactic to skip grace fully for the iteration where branch 1 equates branch 2
+      // skip same branch
       if current_branch_name != other_branch_name {
         let (ahead, behind) = repo.graph_ahead_behind(
           current_branch.get().target().unwrap(),
