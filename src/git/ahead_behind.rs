80edc4ddb72b5b8e4f0a00bf012d65cdbf06d2d0
pudep <261843105+pudep@users.noreply.github.com>
2026-08-08 11:39:48 +0530

    refactor(rename): variables of get_ahead_behind()

--- src/git/ahead_behind.rs
+++ src/git/ahead_behind.rs
@@ -22,7 +22,7 @@ impl Git {
     let mut vector = Vec::new();
     for (_idx, branch) in branches {
       // The current entry of branches that will be matched against branch 1.
-      let secondary_branch = Git::to_branch_local(repo, &branch.as_str())?;
+      let other_branch = Git::to_branch_local(repo, &branch.as_str())?;
 
       // To avoid recalculation everytime
       let current_branch_name = match current_branch.name() {
@@ -30,18 +30,18 @@ impl Git {
         Ok(None) => "<Invalid Utf-8>".to_string(),
         Err(_) => "<Invalid Branch/HEAD is detached>".to_string(),
       };
-      let secondary_branch_name = secondary_branch.name()?.unwrap().to_string();
+      let other_branch_name = other_branch.name()?.unwrap().to_string();
 
       // The filteration tactic to skip grace fully for the iteration where branch 1 equates branch 2
-      if current_branch_name != secondary_branch_name {
+      if current_branch_name != other_branch_name {
         let (ahead, behind) = repo.graph_ahead_behind(
           current_branch.get().target().unwrap(),
-          secondary_branch.get().target().unwrap(),
+          other_branch.get().target().unwrap(),
         )?;
 
         vector.push(ABData {
           given_branch: current_branch_name,
-          other_branch: secondary_branch_name,
+          other_branch: other_branch_name,
           ahead,
           behind,
         });
