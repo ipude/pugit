c2d9ab77ebf247a5534aa26759fccb474927162a
pudep <261843105+pudep@users.noreply.github.com>
2026-08-08 17:19:31 +0530

    feat: add simple return type(`BranchesContainer`) for `Git::get_branches()` to handle every edge case regarding `git2::Branch<'repo>` and `git2::Branches<'repo>`.

--- src/git/branches.rs
+++ src/git/branches.rs
@@ -3,47 +3,111 @@ use std::result::Result;
 use crate::git::Git;
 use git2::Repository;
 
+pub struct BranchesContainer {
+  pub vector_branches: Vec<String>,
+  pub error: BranchesError,
+}
+
+pub struct BranchesError {
+  pub serious: Vec<String>,
+  pub vector_non_utf8_branches: Vec<String>,
+}
+
+impl BranchesContainer {
+  /// Returns `true` if `error.serious` contain at lest one error.
+  pub fn has_serious_err(&self) -> bool {
+    !self.error.serious.is_empty()
+  }
+
+  /// Returns `true` if `error.vector_non_utf8_branches` contain at lest one error.
+  pub fn has_non_utf8_branches(&self) -> bool {
+    !self.error.vector_non_utf8_branches.is_empty()
+  }
+}
+
 #[allow(dead_code)]
 impl Git {
-  /// Returns Error early if `repo.branches()` return error.
-  /// Individual index can contain `Result<branch_name: String, error: String>`
-  pub fn get_branches(
-    repo: &Repository,
-  ) -> Result<(Vec<(usize, String)>, Vec<(usize, String)>), String> {
+  /// Returns [`BranchesContainer`] if iterator of branches [`git2::Branches<'repo>`] can be created.
+  ///
+  /// # BranchesContainer
+  ///
+  /// ```
+  ///pub struct BranchesContainer {
+  ///  // this contains valid branches
+  ///  pub vector_branches: Vec<String>,
+  ///  // this contains malformed branches.
+  ///  pub errors: BranchesError,
+  ///}
+  ///
+  ///pub struct BranchesError {
+  ///   pub serious: Vec<String>,
+  ///   pub vector_non_utf8_branches: Vec<String>,
+  ///}
+  /// ```
+  ///
+  /// # Must use :
+  /// ```
+  ///impl BranchesContainer {
+  ///  /// Returns `true` if `error.serious` contain at lest one error.
+  ///  pub fn has_serious_err(&self) -> bool {
+  ///    !self.error.serious.is_empty()
+  ///  }
+  ///
+  ///  /// Returns `true` if `error.vector_non_utf8_branches` contain at lest one error.
+  ///  pub fn has_non_utf8_branches(&self) -> bool {
+  ///    !self.error.serious.is_empty()
+  ///  }
+  ///}
+  /// ```
+  ///
+  pub fn get_branches(repo: &Repository) -> Result<BranchesContainer, String> {
+    let mut vector_branches: Vec<String> = Vec::new();
+    let mut non_utf_8: Vec<String> = Vec::new();
+    let mut serious: Vec<String> = Vec::new();
+
     // Map error and return early
     let branches = repo
       .branches(Some(git2::BranchType::Local))
       .map_err(|e| e.to_string())?;
-    let mut vector = Vec::new();
 
     for branch in branches {
       let (branch, _btype) = match branch {
         Ok(v) => v,
 
         // Serious error related to malfunctioning of repository.
-        // must store
+        // related to libgit2's core api
         Err(e) => {
-          vector.push(Err(e.to_string()));
+          serious.push(e.to_string());
           continue;
         }
       };
+      match branch.name() {
+        // Pick only valid names in vector_branches.
+        Ok(Some(b)) => vector_branches.push(b.to_string()),
 
-      vector.push(match branch.name() {
-        Err(e) => Err(e.to_string()),
-        Ok(Some(b)) if !b.is_empty() => Ok(b.to_string()),
-        _ => Ok("NA".to_string()),
-      });
-    }
+        // Non utf-8 names must go to vector_errors.
+        Ok(None) => {
+          let lossy_name = String::from_utf8_lossy(branch.get().name_bytes());
+          let oid = branch
+            .get()
+            .target()
+            .map(|o| o.to_string()) // if is Some(o: Oid) then convert to Some(o: String)
+            .unwrap_or_else(|| "<unresolved_oid>".to_string()); // unwrap if Some(o) or else put "<unresolved_oid>"
 
-    let mut oks: Vec<(usize, String)> = Vec::new();
-    let mut errs: Vec<(usize, String)> = Vec::new();
-
-    for (idx, branches) in vector.iter().enumerate() {
-      match branches {
-        Ok(branch) => oks.push((idx, branch.to_string())),
-        Err(error) => errs.push((idx, error.to_string())),
+          // Don't mix with Ok(name) as it must only contain valid branch names.
+          // Send as Err(msg) to avoid sudden runtime crash.
+          non_utf_8.push(format!("{lossy_name}: points to {oid}"))
+        }
+        Err(e) => serious.push(e.to_string()),
       }
     }
-    Ok((oks, errs))
+
+    Ok(BranchesContainer {
+      vector_branches,
+      error: BranchesError {
+        serious,
+        vector_non_utf8_branches: non_utf_8,
+      },
+    })
   }
 }
